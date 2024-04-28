287ca5861f8a18cd59c42e94701680478e8ddbf8
Carlos Garcia <renjcb@msn.com>
2024-04-28 16:16:49 -0700

    rjfflip
    Bitcoins

---
+++ script.js
@@ -0,0 +1,53 @@
+const token = artifacts.require('../contracts/RJFToken.sol')
+const crowdsale = artifacts.require('../contracts/RJFCrowdsale.sol')
+const setDefaultAccount = require('../scripts/setDefaultAccount.js')
+
+module.exports = function(deployer, network, accounts) {
+    const rate = new web3.BigNumber(030)
+    const wallet = '0x281055afc982d96fab65b3a49cac8b878184cb16'
+    const openingTime = (new Date('2020-05-10')).getTime/1000
+    const closingTime = (new Date('2020-05-31')).getTime/1000
+    const goal = 1000
+    const initialRate = 10
+    const finalRate = 1
+    const cap = 1000000
+    // Setup default account
+    setDefaultAccount(web3)
+    const account = web3.eth.accounts.pop()
+    // Get gas limit
+    let gasLimit = web3.eth.getBlock('latest').gasLimit
+    let gasPrice = web3.eth.gasPrice
+    if (process.argv[4] === '--staging') {
+        gasPrice *= 4
+    }
+    console.log(`Determined gas limit: ${gasLimit}; and gas price: ${gasPrice}; max deployment price is ${web3.fromWei(gasPrice * gasLimit, 'ether')} ETH`)
+    // Deploy contract
+    return deployer
+        .then(() => {
+            return deployer.deploy(token, { gas: gasLimit, gasPrice: gasPrice, from: account })
+        })
+        .then(() => {
+            // Get gas limit
+            gasLimit = web3.eth.getBlock('latest').gasLimit
+            console.log(`Determined gas limit: ${gasLimit}; and gas price: ${gasPrice}; estimate max deployment price is ${web3.fromWei(gasPrice * gasLimit, 'ether')} ETH`)
+            console.log('This might take a while, please, be patient')
+            return deployer.deploy(
+                crowdsale,
+                token.address,
+                rate,
+                wallet,
+                openingTime,
+                closingTime,
+                goal,
+                initialRate,
+                finalRate,
+                cap,
+                { gas: gasLimit, gasPrice: gasPrice, from: account },
+            )
+        })
+        .then(() => {
+            // Make smart-contract an owner
+            var tokenContract = web3.eth.contract(token.abi).at(token.address)
+            tokenContract.transferOwnership(crowdsale.address)
+        });
+}
